16c5a0f6a5c6bfffec08108b0b8e7071e957b2bc
aman-459 <aman2007kumar2007@gmail.com>
2026-06-06 22:02:30 +0530

    sumOfThreeNumber

---
+++ javaBasics/sumOfThreeNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sumOfThreeNumber {
+  public static void main (String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter 1st number: ");
+    int a = sc.nextInt();
+    System.out.print("Enter Second number: ");
+    int b = sc.nextInt();
+    System.out.print("Enter third number: ");
+    int c = sc.nextInt();
+    System.out.println("Sum: "+(a+b+c));
+  }
+}
